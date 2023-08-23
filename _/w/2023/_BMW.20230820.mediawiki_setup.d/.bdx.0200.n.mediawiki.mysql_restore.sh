08aa242b5ae731ede5876421d133657cfd3c203a
Brian M. JUNG <brian.m.jung@gmail.com>
2023-08-22 23:59:59 -0700

    _BMM wiki; mediawiki setup (dump, restore)
    	new file:   _/m/_BMM.20230822.wiki_main_page.txt
    	modified:   _/w/2023/_BMW.20230820.mediawiki_setup.d/.bdx.0200.n.mediawiki.mysql_restore.sh
    	renamed:    _/w/2023/_BMW.20230820.mediawiki_setup.d/.files.d/LocalSettings.php -> _/w/2023/_BMW.20230820.mediawiki_setup.d/.files.d/aerosmith/LocalSettings.php
    
    Signed-off-by: Brian M. JUNG <brian.m.jung@gmail.com>

--- _/w/2023/_BMW.20230820.mediawiki_setup.d/.bdx.0200.n.mediawiki.mysql_restore.sh
+++ _/w/2023/_BMW.20230820.mediawiki_setup.d/.bdx.0200.n.mediawiki.mysql_restore.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-_sql_cmd_file=".tmp.mysql_restore.sql";
-cat > $_sql_cmd_file << EOF
-show databases;
-show tables from matrix;
-EOF
-#drop matrix;
-#create database matrix;
 
+_sql_dump_dir="bxd_obj_mediawiki";
+
+_sql_file_for_prep=".tmp.mysql_restore.prep.sql";
+cat > $_sql_file_for_prep << EOF
+drop matrix;
+create database matrix;
+EOF
+#drop matrix_radiohead;
+#create database matrix_radiohead;
+
+echo "1. Prep";
 echo "____________";
-cat $_sql_cmd_file;
+cat $_sql_file_for_prep;
 echo "____________";
-read -p "Do you want to execute this SQL statements? [Y|n] " _answer;
+read -p "Do you want to execute this SQL commands? [Y|n] " _answer;
 if [ "X$_answer" = "Xy" ]; then
-	echo "mysql -p -u neo < $_sql_cmd_file > result.log";
-	mysql -p -u neo < $_sql_cmd_file > result.log
+	echo "mysql -p -u neo < $_sql_file_for_prep > result.log";
+	mysql -p -u neo < $_sql_file_for_prep > result.log
+else
+	echo "Nothing happened"
+fi
+
+
+echo "2. Main (restore DB with SQL dump file)";
+echo "____________";
+ls -alF ${_sql_dump_dir}/;
+echo "____________";
+_sql_dumpfile_for_restore=$(cd $_sql_dump_dir; ls -1t mediawiki_db_dump.*.sql | head -1);
+echo $_sql_dumpfile_for_restore;
+echo "____________";
+read -p "Do you want to restore DB with SQL dump file above? [Y|n] " _answer;
+if [ "X$_answer" = "Xy" ]; then
+	echo "mysql -p -u neo < ${_sql_dump_dir}/${_sql_dumpfile_for_restore} > result.log";
+	mysql -p -u neo < ${_sql_dump_dir}/${_sql_dumpfile_for_restore} > result.log
 else
 	echo "Nothing happened"
 fi
