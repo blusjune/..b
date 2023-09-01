7b15f44b24b6196ef4ef11349bb06cc4435618e2
Brian M. JUNG <brian.m.jung@gmail.com>
2023-09-01 01:21:36 -0700

    .bdx:fnbx.copy_src_here.src_name.sh
    Signed-off-by: Brian M. JUNG <brian.m.jung@gmail.com>

---
+++ _/d/__ROOT__/home/blusjune/.vimrc.old
@@ -0,0 +1,12 @@
+" 20230701_103934
+set nu ai cin
+set nocompatible
+set ignorecase
+"Please
+"Open this .vimrc file, and
+"execute the ex command :PlugInstall
+call plug#begin()
+Plug 'aklt/plantuml-syntax'             " syntax hl for puml
+Plug 'tyru/open-browser.vim'            " hooks for opeing browser
+Plug 'weirongxu/plantuml-previewer.vim' " previewer
+call plug#end()
--- _/x/.bdx.0100.y.fnbx.copy_src_here.src_name.sh
+++ _/x/.bdx.0100.y.fnbx.copy_src_here.src_name.sh
@@ -6,7 +6,13 @@
 ###                                       ".2  .3   .4.5   .6            .7       .8 "
 
 _this_file_name="$( basename $( echo $0 ) )";
-_src="$( echo $_this_file_name | awk -F'.' '{ if ($4 == "y") { print $7 }}' )";
+#_src="$( echo $_this_file_name | awk -F'.' '{ if ($4 == "y") { print $7 }}' )";
+_src="$( echo $_this_file_name | awk -F'.y.fnbx.copy_src_here.' '{ print $2 }' | sed -e 's/\(.*\)\.sh/\1/g' )";
+echo $_src;
+if [ "X$_src" = "X" ]; then
+	echo "ERROR:: empty _src"
+	exit;
+fi
 _dir_base="$( pwd | awk -F'__ROOT__' '{ print $2 }' )";
 
 set -x;
