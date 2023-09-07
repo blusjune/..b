0d65c2f668d9cf256f755f9b9f0416596f506e3a
Brian M. JUNG <brian.m.jung@gmail.com>
2023-09-07 01:22:51 -0700

    .bdx:AIWB (AI Work Bench)
    	new file:   .ddb.pkg.d/.ddb.pkg._.20230907_012236.tar.gz
    	modified:   _/d/__ROOT__/home/blusjune/GNUstep/.AppInfo/WindowMaker/History
    	modified:   _/m/_BMM.20230904.ai_hf_huggingface.txt
    	new file:   _/w/2023/_BMW.20230904.ai_llm_hugging_face.d/hf
    	new file:   _/w/2023/_BMW.20230904.ai_llm_hugging_face.d/huggingface_src
    	new file:   _/x/.bdx.0100.y.aiwb.nvidia.pkg.install.sh
    	new file:   _/x/.bdx.0110.y.aiwb.huggingface.pkg.install.sh
    	modified:   _/x/.bx.swpkg.install.sh
    
    Signed-off-by: Brian M. JUNG <brian.m.jung@gmail.com>

---
+++ _/x/.bdx.0110.y.aiwb.huggingface.pkg.install.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+# AIWB: Artificial Intelligence Work Bench
+# .bdx.0110.y.aiwb.huggingface.pkg.install.sh
+# 20230907_002829
+_corename="aiwb.huggingface.pkg.install";
+_thisprog=".bdx.0110.y.${_corename}.sh"
+
+
+_ts="date +%Y%m%d_%H%M%S";
+declare -A _work_step_file;
+_step=0;
+_exe_log=".exelog.${_corename}.$($_ts).log";
+
+
+
+
+#######################
+### prerequisites { ###
+_hf_token=".tmp.hf_token.txt";
+_hf_token_gpg=".tmp.hf_token.txt.gpg";
+cat > $_hf_token_gpg << EOF_HF_TOKEN
+-----BEGIN PGP MESSAGE-----
+Comment: BGPGFile:20230904_175119 by B.
+
+jA0ECQMCUPsM6YCDr6f/0mcBAOGFKOGtcQlMttnT2Chx8VNlIrnFk2PJtKDFxvWN
+0bwD5/ooiwRyV4ikZWLzCzoF20ENpO4pAW1IyGDsV88Uqa/lLFhHwCx+5I/7RhFS
+/m3oNFnUW9Z2avG4zXrbDF8q9Hv5Pthp
+=dPZP
+-----END PGP MESSAGE-----
+EOF_HF_TOKEN
+gpg $_hf_token_gpg;
+cat $_hf_token
+# huggingface-cli login;
+### } prerequisites ###
+#######################
+
+
+
+
+### step++
+_step=$((_step + 1));
+_work_step_file[$_step]=".workstep.${_corename}.$(printf '%08d' $_step).sh";
+cat > ${_work_step_file[$_step]} << EOF_WORK_STEP_COMMANDS
+#!/bin/bash
+### workstep[$_step];
+echo "hello, world -- this is workstep"
+EOF_WORK_STEP_COMMANDS
+chmod 755 ${_work_step_file[$_step]};
+
+
+### step++
+_step=$((_step + 1));
+_work_step_file[$_step]=".workstep.${_corename}.$(printf '%08d' $_step).sh";
+cat > ${_work_step_file[$_step]} << EOF_WORK_STEP_COMMANDS
+#!/bin/bash
+### workstep[$_step];
+git clone https://github.com/huggingface/huggingface_hub.git;
+(cd huggingface_hub;
+	pip install -e .;
+)
+EOF_WORK_STEP_COMMANDS
+chmod 755 ${_work_step_file[$_step]};
+
+
+### step++
+_step=$((_step + 1));
+_work_step_file[$_step]=".workstep.${_corename}.$(printf '%08d' $_step).sh";
+cat > ${_work_step_file[$_step]} << EOF_WORK_STEP_COMMANDS
+#!/bin/bash
+### workstep[$_step];
+(cd huggingface_hub;
+	sudo apt install python3.10-venv;
+	python -m venv .env;
+	source .env/bin/activate;
+	pip install --upgrade huggingface_hub;
+)
+EOF_WORK_STEP_COMMANDS
+chmod 755 ${_work_step_file[$_step]};
+
+
+echo "";
+echo "### Execution log will be saved in $_exe_log";
+_i=0;
+while [ $_i -lt $_step ]; do
+	_i=$((_i + 1));
+	echo "";
+	echo "";
+	echo "====================================================";
+	echo "step[ $_i ]: " ${_work_step_file[$_i]};
+	echo "----------------------------------------------------";
+	cat ${_work_step_file[$_i]};
+	echo "----------------------------------------------------";
+	read -p ">>> Execute? [y(yes)|S(skip)|t(terminate)] " _answer;
+	case $_answer in 
+		"y"|"Y")
+			echo "### $($_ts) :EXEC: phase[ $_i ]: " ${_work_step_file[$_i]} | tee -a $_exe_log;
+			echo "____________________________________________________";
+			${_work_step_file[$_i]};
+			;;
+		"s"|"S"|"n"|"N")
+			echo "### $($_ts) :SKIP: phase[ $_i ]: " ${_work_step_file[$_i]} | tee -a $_exe_log;
+			;;
+		"t"|"T")
+			echo "### $($_ts) :TERM: terminate this installation loop" | tee -a $_exe_log;
+			break;
+			;;
+		*)
+			echo "### $($_ts) :SKIP: phase[ $_i ]: " ${_work_step_file[$_i]} | tee -a $_exe_log;
+			;;
+	esac
+	echo "____________________________________________________";
+	echo "";
+	echo "";
+done
--- _/x/.bx.swpkg.install.sh
+++ _/x/.bx.swpkg.install.sh
@@ -24,7 +24,7 @@ proftpd ncftp
 exuberant-ctags cscope
 ipython3 python3-ipython python3-ipython-genutils python3-pip idle idle-python3.10 pyprof2calltree
 make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl cmake
-plocate
+plocate dkms
 "
 
 
