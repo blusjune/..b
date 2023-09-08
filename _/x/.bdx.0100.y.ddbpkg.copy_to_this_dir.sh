604def256d156c5112de8b99c27c68be8c279ea2
Brian M. JUNG <brian.m.jung@gmail.com>
2023-09-08 00:15:12 -0700

    _/x/.bdx.0100.y.ddbpkg.copy_to_this_dir.sh
    	modified:   _/x/.bdx.0100.y.ddbpkg.copy_to_this_dir.sh
    	_SLEEP_AFTER_DONE="y"
    
    Signed-off-by: Brian M. JUNG <brian.m.jung@gmail.com>

--- _/x/.bdx.0100.y.ddbpkg.copy_to_this_dir.sh
+++ _/x/.bdx.0100.y.ddbpkg.copy_to_this_dir.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # 20230907_075118
 
-if [ "X${_SLEEP_AFTER_DONE}" = "Xyes" ]; then
+
+if [ "X${_SLEEP_AFTER_DONE}" = "Xy" ]; then
 	echo "### system will be hibernated (because _SLEEP_AFTER_DONE is '$_SLEEP_AFTER_DONE')";
 else
 	echo "### system will not be hibernated (because _SLEEP_AFTER_DONE is '$_SLEEP_AFTER_DONE')";
@@ -11,22 +12,33 @@ if [ "X$_answer" != "Xy" ]; then
 	echo "### nothing happended -- exit 0";
 	exit 0;
 fi
+
+
 sleep 3;
+
 
 _ts="date +%Y%m%d_%H%M%S";
 _tsnapshot=$($_ts);
+
+
 set -x;
-echo $_tsnapshot > .bdx.${_tsnapshot}.log;
+
+
 _tgt_dir=$(pwd); # here
-cd ~blusjune/..bxd;
+echo $_tsnapshot > .bdx.${_tsnapshot}.log;
+
 echo "### $($_ts): bxd tar ball - start"  > .bdx.${_tsnapshot}.log;
-./.bdx.0100.y.ddb.pkg.sh;
+( cd ~blusjune/..bxd;
+./.bdx.0100.y.ddb.pkg.sh; # .ddb.pkg.sh
+)
+
 echo "### $($_ts): bxd file copy - start"  > .bdx.${_tsnapshot}.log;
-(cd  ~/..bxd/.ddb.pkg.d/; 
+( cd  ~blusjune/..bxd/.ddb.pkg.d/; 
 	_src_file=$(ls -1 .ddb.pkg.*.tar.gz | tail -1);
 	cp -p $_src_file $_tgt_dir;
 )
+
 echo "### $($_ts): all done"  > .bdx.${_tsnapshot}.log;
-if [ "X${_SLEEP_AFTER_DONE}" = "Xyes" ]; then
+if [ "X${_SLEEP_AFTER_DONE}" = "Xy" ]; then
 	sudo systemctl hibernate;
 fi
