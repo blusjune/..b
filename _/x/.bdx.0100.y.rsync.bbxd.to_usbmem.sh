81c4ebc58c97a79194257fb3f686c4d43b1cd5e1
Brian M. JUNG <brian.m.jung@gmail.com>
2023-09-30 11:20:19 -0700

    20230930_112023
    BDX:rsync.bbxd.to_usbmem
    
    	deleted:    _/x/.bdx.0100.y.rsync.bbxd.to_ssd_0t5_t5_oasis.sh
    	new file:   _/x/.bdx.0100.y.rsync.bbxd.to_usbmem.sh
    
    Signed-off-by: Brian M. JUNG <brian.m.jung@gmail.com>

---
+++ _/x/.bdx.0100.y.rsync.bbxd.to_usbmem.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# .bdx.0100.y.rsync.bbxd.to_usbmem.sh # 20230929_224212
+# .bdx.0100.y.rsync.bbxd.to_ssd_0t5_t5_oasis.sh # 20230912_220510
+
+
+source /_b/x/.blib.rsync.sh;
+
+
+_mnt_root="/mnt/media.blusjune";
+_usbmem_oasis=".ssd_0t5_t5_oasis";
+_usbmem_blur=".ssd_1t0_t7_blur";
+
+
+function _rsync_from_cwd_to_usbmem()
+{
+#_mnt_root="/mnt/media.blusjune";
+#_usbmem_tgt="$_usbmem_blur";
+	if [ -d ${_mnt_root}/${_usbmem_tgt} ]; then
+		echo "### ===================================================================================";
+		echo "### ===================================================================================";
+		echo "###";
+		echo "### INF: rsync to  ${_mnt_root}/${_usbmem_tgt}";
+		echo "###";
+		echo "### ___________________________________________________________________________________";
+		_list_to_rsync="..b ..bxd .bigfiles.d";
+		_rsync_target_dir_root="${_mnt_root}/${_usbmem_tgt}/..__ROOT__";
+		_rsync_opt="-v --delete";
+		rsync_exec;
+		#
+		_list_to_rsync=".config";
+		_rsync_target_dir_root="${_mnt_root}/${_usbmem_tgt}/..__ROOT__/home.blusjune.$(hostname)";
+		_rsync_opt="-v --delete";
+		rsync_exec;
+	fi
+}
+
+
+_usbmem_tgt="$_usbmem_oasis";
+_rsync_from_cwd_to_usbmem;
+
+
+_usbmem_tgt="$_usbmem_blur";
+_rsync_from_cwd_to_usbmem;
+
+
