dda11c2105d229e81d4ec8bcedb22c0ec213be82
Brian M. JUNG <brian.m.jung@gmail.com>
2023-11-18 16:42:30 -0800

    20231118_164251
    BDX:wget.mirror.thenewyorker
    
    	new file:   _/w/2023/_BMW.20231118.mirroring_websites.d/..README.._BMW.20231118.mirroring_websites.d
    	new file:   _/w/2023/_BMW.20231118.mirroring_websites.d/.bxd
    	new file:   _/w/2023/_BMW.20231118.mirroring_websites.d/websites
    	new file:   _/w/x/mirroring_websites
    	modified:   _/x/.bdx.0100.y.wget.mirror.thenewyorker.sh
    
    Signed-off-by: Brian M. JUNG <brian.m.jung@gmail.com>

--- _/x/.bdx.0100.y.wget.mirror.thenewyorker.sh
+++ _/x/.bdx.0100.y.wget.mirror.thenewyorker.sh
@@ -5,15 +5,37 @@
 
 
 
+_basic_options=" --mirror --page-requisites --convert-links --adjust-extension --span-hosts ";
+#	--mirror (-m)
+#	--page-requisites (-p)
+#	--convert-links (-k)
+#	--adjust-extension (-E)
+#	--span-hosts (-H)
+#	--no-parent (-np)
+#	--backup-converted (-K)
+#	--D domain-list
+
 _user_agent="Brave/114.1.52.126 Google Chrome/114.0.5735.134 Internet Explorer/11.00.19041.1 Microsoft Edge/114.0.1823.51";
 _target_website="http://www.newyorker.com/";
-#_domain_list="www.newyorker.com,contest.newyorker.com,downloads.newyorker.com,media.newyorker.com,newyorker.com,store.newyorker.com,subscribe.newyorker.com,video.newyorker.com";
-_domain_list="newyorker.com";
+_domain_list="newyorker.com"; #_domain_list="www.newyorker.com,contest.newyorker.com,downloads.newyorker.com,media.newyorker.com,newyorker.com,store.newyorker.com,subscribe.newyorker.com,video.newyorker.com";
 
 
-wget -mpHkKEb -t 1 -e robots=off -U $_user_agent -D $_domain_list $_target_website
+
+
+wget $_basic_options -t 1 -e robots=off -U $_user_agent $_target_website
+#wget $_basic_options -t 1 -e robots=off -U $_user_agent -D $_domain_list $_target_website
+
+
+
+
+###
+### History: not satisfactory options
+###
+#wget -mpHkKEb -t 1 -e robots=off -U $_user_agent -D $_domain_list $_target_website
 #wget -mpkKEb -t 1 -e robots=off -U $_user_agent -D $_domain_list $_target_website
 #wget -mpHkKEb -t 1 -e robots=off -U $_user_agent $_target_website
 #wget -mpHkKEb -t 1 -e robots=off -U "Brave/114.1.52.126 Google Chrome/114.0.5735.134 Internet Explorer/11.00.19041.1 Microsoft Edge/114.0.1823.51" http://www.newyorker.com/
+
+
 
 
