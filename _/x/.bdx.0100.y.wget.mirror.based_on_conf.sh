3ee4014e6d3dd2808068f09fe80fa73f649488ae
Brian M. Aerosmith JUNG <brian.m.jung@gmail.com>
2024-01-27 11:01:17 -0800

    20240127_110139
    	modified:   _/x/.bdx.0100.y.wget.mirror.based_on_conf.sh
    
    Signed-off-by: Brian M. Aerosmith JUNG <brian.m.jung@gmail.com>

--- _/x/.bdx.0100.y.wget.mirror.based_on_conf.sh
+++ _/x/.bdx.0100.y.wget.mirror.based_on_conf.sh
@@ -11,8 +11,26 @@ _bdx_conf_file=".bdx_conf.wget.mirror.txt";
 
 
 
+if [ ! -f $_bdx_conf_file ]; then
+	cat > $_bdx_conf_file << EOF_CONF
+# bdx conf file for .bdx.0100.y.wget.mirror.based_on_conf.sh
+# $($_ts)
+_target_website="http://www.newyorker.com/";
+_domain_list="newyorker.com,www.wnyc.org";
+_base_url="http://localhost/mirror/newyorker/";
+_user_agent="Brave/114.1.52.126 Google Chrome/114.0.5735.134 Internet Explorer/11.00.19041.1 Microsoft Edge/114.0.1823.51";
+EOF_CONF
+fi
+echo "#________________________________________________________________";
+cat $_bdx_conf_file;
+echo "#________________________________________________________________";
+
+
+
+
 _basic_options=" --mirror --background --page-requisites --convert-links --adjust-extension --span-hosts ";
 #_basic_options=" --mirror --background --page-requisites --convert-links --adjust-extension --span-hosts -w 1 --random-wait ";
+#_basic_options=" --mirror --background --page-requisites --convert-links --adjust-extension --span-hosts -w 1 --random-wait -F -B ${_base_url} ";
 #	--mirror (-m)
 #	--background (-b)
 #	--page-requisites (-p)
@@ -22,25 +40,11 @@ _basic_options=" --mirror --background --page-requisites --convert-links --adjus
 #	--no-parent (-np)
 #	--backup-converted (-K)
 #	-w 1
+#	--force-html (-F)
+#	--base=_base_url (-B _base_url)
 #	--random-wait
 #	--D domain-list
-_user_agent="Brave/114.1.52.126 Google Chrome/114.0.5735.134 Internet Explorer/11.00.19041.1 Microsoft Edge/114.0.1823.51";
-
-
-
-
-if [ ! -f $_bdx_conf_file ]; then
-	cat > $_bdx_conf_file << EOF_CONF
-# bdx conf file for .bdx.0100.y.wget.mirror.based_on_conf.sh
-# $($_ts)
-_target_website="http://www.newyorker.com/";
-_domain_list="newyorker.com,www.wnyc.org";
-EOF_CONF
-fi
-#
-echo "#________________________________________________________________";
-cat $_bdx_conf_file;
-echo "#________________________________________________________________";
+#	-U _user_agent
 
 
 
